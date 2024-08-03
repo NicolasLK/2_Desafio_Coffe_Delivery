2ad2375b18ed97df5bbd6fa68ecb56a367cc8df4
NicolasLK <nicolas.kaminski.2001@gmail.com>
2024-08-03 18:17:42 -0300

    feat: Criando navegacao entre carrinho e pedido completo

--- src/components/completeOrder/SelectedCoffeesForm.tsx
+++ src/components/completeOrder/SelectedCoffeesForm.tsx
@@ -1,11 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import { CustomButton } from "../CustomButton";
 import { CoffeeItem } from "./CoffeeItem";
+import { ValueCartForm } from "./ValueCartForm";
 
 export const SelectedCoffeesForm = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <h1 className="mb-4 font-Baloo2_Bold text-titleXS">Cafés selecionados</h1>
-      <section className="p-10 rounded-tl-md rounded-bl-[2.75rem] rounded-tr-[2.75rem] rounded-br-md mb-2 bg-base-card">
+      <section className="flex flex-col gap-6 p-10 rounded-tl-md rounded-bl-[2.75rem] rounded-tr-[2.75rem] rounded-br-md mb-2 bg-base-card">
         <CoffeeItem />
+        <CoffeeItem />
+        <div className="flex flex-col gap-3">
+          <ValueCartForm />
+        </div>
+        <CustomButton
+          onClick={() => {
+            navigate("/pedido_enviado");
+          }}
+          className="px-2 py-3 rounded-md bg-product-yellow font-Roboto_Bold text-buttonG text-base-white uppercase"
+        >
+          confirmar pedido
+        </CustomButton>
       </section>
     </>
   );
---
+++ src/components/completeOrder/ValueCartForm.tsx
@@ -0,0 +1,28 @@
+export const ValueCartForm = () => {
+  return (
+    <>
+      <span className="flex items-center justify-between">
+        <p className="font-Roboto_Regular text-textS text-base-text">
+          Total de itens
+        </p>
+        <p className="font-Roboto_Regular text-textM text-base-text">
+          R$ 29,70
+        </p>
+      </span>
+      <span className="flex items-center justify-between">
+        <p className="capitalize font-Roboto_Regular text-textS text-base-text">
+          entrega
+        </p>
+        <p className="font-Roboto_Regular text-textM text-base-text">
+          R$ 29,70
+        </p>
+      </span>
+      <span className="flex items-center justify-between">
+        <p className="capitalize font-Roboto_Bold text-textL text-base-text">
+          total
+        </p>
+        <p className="font-Roboto_Bold text-textL text-base-text">R$ 29,70</p>
+      </span>
+    </>
+  );
+};
